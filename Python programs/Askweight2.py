46c2aa96e309f9b2a44bcfd4b229906d8bcb85d7
arminbih <arminbecirspahic@gmail.com>
2019-12-12 20:09:03 +0100

    new program
    asa

--- Python programs/Askweight2.py
+++ Python programs/Askweight2.py
@@ -1,3 +1,4 @@
 weightpd = input('what is your weight in pounds ')
 weightkg = int(weightpd) * 0.45
 print (weightkg)
+#test
---
+++ Python programs/passingrefference.py
@@ -0,0 +1,5 @@
+def eggs(some_paramether):
+    some_paramether.append("Hello")
+spam = [1, 2, 3]
+eggs = (spam)
+print (spam)
